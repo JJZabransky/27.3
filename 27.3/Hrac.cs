2209bb496511d5c5ecc0df789e99c1189729a2bd
Jan Jakub Zábranský <zabransky@SPSEJECNANET.CZ>
2023-03-27 09:10:21 +0200

    second commit

---
+++ 27.3/Hrac.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27._3
+{
+    public class Hrac
+    {
+        private string name;
+        private int hp;
+        public bool isAlive;
+
+        public string Name { get { return name; } set { name = value; } }
+        public int HP { get { return hp; } set { hp = value; } }
+
+        public Hrac(string name, int hp)
+        {
+            this.name = name;
+            this.hp = hp;
+        }
+
+        public void Zvysit(int n)
+        {
+            lock()
+            {
+                if (isAlive == true)
+                {
+                    hp += n;
+                }
+            }
+        }
+
+        public void Snizit(int n)
+        {
+            lock()
+            {
+                if (isAlive == true)
+                {
+                    hp -= n;
+                }
+            }
+        }
+    }
+}
--- 27.3/Program.cs
+++ 27.3/Program.cs
@@ -2,9 +2,42 @@
 {
     internal class Program
     {
+        public static Hrac h = new Hrac("Bruzek", 50);
+        public static Random rd = new Random();
+        public static string filePath = "C:\\Users\\zabransky\\Downloads\\dluznici.csv";
         static void Main(string[] args)
         {
-            
+            Thread t1 = new Thread(new ThreadStart(Zvys));
+            Thread t2 = new Thread(new ThreadStart(Sniz));
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+
+            Console.WriteLine(h.HP + " " + h.isAlive);
+        }
+
+        public static void Zvys()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                h.Zvysit(rd.Next(50));
+            }
+        }
+
+        public static void Sniz()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                 h.Snizit(rd.Next(50));
+            }
+        }
+
+        public void FindDluznik(int id)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            string line = sr.ReadLine();
+
         }
     }
 }
